1639fc52eb9b0591bfd1659352d650ad709ddef2
acro5212 <83582809+acro5212@users.noreply.github.com>
2021-05-03 20:35:52 +1000

    Add files via upload

---
+++ MyClassifier.py
@@ -0,0 +1,18 @@
+##ThreeDigits
+import sys
+
+# from nodes import Node
+# from tree import Tree
+
+#print(algorithm)
+
+training_data = open(sys.argv[1], "r")
+training = training_data.readlines()
+
+testing_data = open(sys.argv[2], "r")
+testing = testing_data.readlines()
+
+algorithm = sys.argv[3]
+
+
+
