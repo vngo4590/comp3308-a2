99be81c6d243331edfbd114e168763401e4bc1e0
Aaron Ngo <aaron.vngo@gmail.com>
2021-05-11 16:27:29 +1000

    added density function

--- MyClassifier.py
+++ MyClassifier.py
@@ -49,6 +49,16 @@ class MyClassifier:
         if n <= 1:
             return 0
         return math.sqrt(sum_std/(n-1))
+    def find_density(self, column_no:int, x:numbers.Number):
+        # test if the values are within bound
+        if self.training_data == None or column_no < 0 or column_no >= len(self.training_data):
+            return None
+        mean = self.find_mean(column_no)
+        std_deviation = self.find_standard_deviation(column_no)
+        factor = 1/(std_deviation*math.sqrt(2*math.pi))
+        exp_hat = math.exp((-1*pow((x-mean), 2))/(2*pow(std_deviation,2)))
+        return factor*exp_hat
+    
     
 #print(algorithm)
 '''
@@ -59,5 +69,5 @@ if len(sys.argv) != 4:
 else :
     # Declare the class & then execute
     solution = MyClassifier(sys.argv[1].strip(), sys.argv[2].strip(), sys.argv[3].strip())
-    print(solution.find_standard_deviation(1))
+    print(solution.find_density(1, 0.16))
     
